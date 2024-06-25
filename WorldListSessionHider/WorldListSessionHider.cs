1bdef16dffeea03cb847918a583df1af0a073daf
Nytra <14206961+Nytra@users.noreply.github.com>
2024-06-25 22:53:51 +0100

    Fix for new update, add world ID filter, improve config string parsing

--- WorldListSessionHider/WorldListSessionHider.cs
+++ WorldListSessionHider/WorldListSessionHider.cs
@@ -15,7 +15,7 @@ namespace WorldListSessionHider
 	{
 		public override string Name => "WorldListSessionHider";
 		public override string Author => "Nytra";
-		public override string Version => "1.1.1";
+		public override string Version => "1.2.0";
 		public override string Link => "https://github.com/Nytra/ResoniteWorldListSessionHider";
 
 		public static ModConfiguration Config;
@@ -27,7 +27,7 @@ namespace WorldListSessionHider
 		[AutoRegisterConfigKey]
 		private static ModConfigurationKey<dummy> DUMMY_0_1 = new ModConfigurationKey<dummy>("DUMMY_0_1", "<color=green>[FILTERS]</color>", () => new dummy());
 		[AutoRegisterConfigKey]
-		private static ModConfigurationKey<bool> HIDE_FILTERED_SESSIONS = new ModConfigurationKey<bool>("HIDE_FILTERED_SESSIONS", "Use filters:", () => false);
+		private static ModConfigurationKey<bool> USE_FILTERS = new ModConfigurationKey<bool>("USE_FILTERS", "Use filters:", () => true);
 		[AutoRegisterConfigKey]
 		private static ModConfigurationKey<dummy> DUMMY_0_2 = new ModConfigurationKey<dummy>("DUMMY_0_2", "<size=0></size>", () => new dummy());
 		[AutoRegisterConfigKey]
@@ -37,17 +37,45 @@ namespace WorldListSessionHider
 		[AutoRegisterConfigKey]
 		private static ModConfigurationKey<string> SESSION_IDS = new ModConfigurationKey<string>("SESSION_IDS", "Session IDs to filter:", () => "");
 		[AutoRegisterConfigKey]
+		private static ModConfigurationKey<string> WORLD_IDS = new ModConfigurationKey<string>("WORLD_IDS", "Composite World Record IDs / World Record URLs to filter:", () => "");
+		[AutoRegisterConfigKey]
 		private static ModConfigurationKey<dummy> DUMMY_1 = new ModConfigurationKey<dummy>("DUMMY_1", "<i><color=gray>All of these can be comma-separated to store multiple values.</color></i>", () => new dummy());
 		[AutoRegisterConfigKey]
-		private static ModConfigurationKey<dummy> DUMMY_2 = new ModConfigurationKey<dummy>("DUMMY_2", "<i><color=gray>e.g: U-Cheese,U-spaghet,U-OwO</color></i>", () => new dummy());
+		private static ModConfigurationKey<dummy> DUMMY_1_1 = new ModConfigurationKey<dummy>("DUMMY_1_1", "<i><color=gray>e.g: U-Cheese, U-spaghet, U-OwO</color></i>", () => new dummy());
+
+		// session ID examples
+
 		[AutoRegisterConfigKey]
-		private static ModConfigurationKey<dummy> DUMMY_2_1 = new ModConfigurationKey<dummy>("DUMMY_2_1", "<size=0></size>", () => new dummy());
+		private static ModConfigurationKey<dummy> DUMMY_1_1_1b = new ModConfigurationKey<dummy>("DUMMY_1_1_1b", "<size=0></size>", () => new dummy());
+		[AutoRegisterConfigKey]
+		private static ModConfigurationKey<dummy> DUMMY_1_2b = new ModConfigurationKey<dummy>("DUMMY_1_2b", "<i><color=gray>Acceptable Session ID Examples:</color></i>", () => new dummy());
+		[AutoRegisterConfigKey]
+		private static ModConfigurationKey<dummy> DUMMY_1_2_1b = new ModConfigurationKey<dummy>("DUMMY_1_2_1b", "<i><color=gray>S-U-Nytra:AmazingSession</color></i>", () => new dummy());
+		[AutoRegisterConfigKey]
+		private static ModConfigurationKey<dummy> DUMMY_1_2_2b = new ModConfigurationKey<dummy>("DUMMY_1_2_2b", "<i><color=gray>S-blah-blah</color></i>", () => new dummy());
+
+		// world ID examples
+
+		[AutoRegisterConfigKey]
+		private static ModConfigurationKey<dummy> DUMMY_1_1_1 = new ModConfigurationKey<dummy>("DUMMY_1_1_1", "<size=0></size>", () => new dummy());
+		[AutoRegisterConfigKey]
+		private static ModConfigurationKey<dummy> DUMMY_1_2 = new ModConfigurationKey<dummy>("DUMMY_1_2", "<i><color=gray>Acceptable World ID Examples:</color></i>", () => new dummy());
+		[AutoRegisterConfigKey]
+		private static ModConfigurationKey<dummy> DUMMY_1_2_1 = new ModConfigurationKey<dummy>("DUMMY_1_2_1", "<i><color=gray>resrec:///U-Nytra/R-blah-blah</color></i>", () => new dummy());
+		[AutoRegisterConfigKey]
+		private static ModConfigurationKey<dummy> DUMMY_1_2_2 = new ModConfigurationKey<dummy>("DUMMY_1_2_2", "<i><color=gray>U-Nytra/R-blah-blah</color></i>", () => new dummy());
+		[AutoRegisterConfigKey]
+		private static ModConfigurationKey<dummy> DUMMY_1_2_3 = new ModConfigurationKey<dummy>("DUMMY_1_2_3", "<i><color=gray>U-Nytra:R-blah-blah</color></i>", () => new dummy());
+
+
+		[AutoRegisterConfigKey]
+		private static ModConfigurationKey<dummy> DUMMY_1_3 = new ModConfigurationKey<dummy>("DUMMY_1_3", "<size=0></size>", () => new dummy());
 		[AutoRegisterConfigKey]
 		private static ModConfigurationKey<bool> HIDE_FILTERED_SESSIONS_COMPLETELY = new ModConfigurationKey<bool>("HIDE_FILTERED_SESSIONS_COMPLETELY", "Hide filtered sessions completely:", () => false);
 		[AutoRegisterConfigKey]
-		private static ModConfigurationKey<dummy> DUMMY_3 = new ModConfigurationKey<dummy>("DUMMY_3", "<size=0></size>", () => new dummy());
+		private static ModConfigurationKey<dummy> DUMMY_2 = new ModConfigurationKey<dummy>("DUMMY_2", "<size=0></size>", () => new dummy());
 		[AutoRegisterConfigKey]
-		private static ModConfigurationKey<dummy> DUMMY_3_1 = new ModConfigurationKey<dummy>("DUMMY_3_1", "<color=green>[STUCK SESSIONS]</color>", () => new dummy());
+		private static ModConfigurationKey<dummy> DUMMY_2_1 = new ModConfigurationKey<dummy>("DUMMY_2_1", "<color=green>[STUCK SESSIONS]</color>", () => new dummy());
 		[AutoRegisterConfigKey]
 		private static ModConfigurationKey<bool> HIDE_DEAD_SESSIONS = new ModConfigurationKey<bool>("HIDE_DEAD_SESSIONS", "Hide stuck sessions:", () => false);
 		//[AutoRegisterConfigKey]
@@ -55,9 +83,9 @@ namespace WorldListSessionHider
 		[AutoRegisterConfigKey]
 		private static ModConfigurationKey<bool> HIDE_DEAD_SESSIONS_COMPLETELY = new ModConfigurationKey<bool>("HIDE_DEAD_SESSIONS_COMPLETELY", "Hide stuck sessions completely:", () => false);
 		[AutoRegisterConfigKey]
-		private static ModConfigurationKey<dummy> DUMMY_4 = new ModConfigurationKey<dummy>("DUMMY_4", "<i><color=gray>A stuck session is one that shows up in the browser but doesn't actually exist and cannot be joined.</color></i>", () => new dummy());
+		private static ModConfigurationKey<dummy> DUMMY_2_2 = new ModConfigurationKey<dummy>("DUMMY_2_2", "<i><color=gray>A stuck session is one that shows up in the browser but doesn't actually exist and cannot be joined.</color></i>", () => new dummy());
 		[AutoRegisterConfigKey]
-		private static ModConfigurationKey<dummy> DUMMY_4_1 = new ModConfigurationKey<dummy>("DUMMY_4_1", "<i><color=orange>It is unknown if this feature is actually useful in Resonite, so it is turned off by default.</color></i>", () => new dummy());
+		private static ModConfigurationKey<dummy> DUMMY_2_3 = new ModConfigurationKey<dummy>("DUMMY_2_3", "<i><color=orange>It is unknown if this feature is actually useful in Resonite, so it is turned off by default.</color></i>", () => new dummy());
 		//[AutoRegisterConfigKey]
 		//private static ModConfigurationKey<dummy> DUMMY_6 = new ModConfigurationKey<dummy>("DUMMY_6", "<size=0></size>", () => new dummy());
 		//[AutoRegisterConfigKey]
@@ -96,26 +124,28 @@ namespace WorldListSessionHider
 					{
 						Debug($"SessionID: \"{s}\"");
 					}
+					foreach (string s in Config.GetValue(WORLD_IDS)?.Split(','))
+					{
+						Debug($"WorldID: \"{s}\"");
+					}
 					Debug("Done.");
 				}
 			};
 			harmony.PatchAll();
 		}
 
-		private static Type theType = typeof(WorldThumbnailItem);
+		private static Type worldThumbnailItemType = typeof(LegacyWorldThumbnailItem);
 
-		private static MethodInfo getBestSessionMethod = AccessTools.Method(typeof(WorldItem), "GetBestSession");
+		private static MethodInfo getBestSessionMethod = AccessTools.Method(typeof(LegacyWorldItem), "GetBestSession");
 
-		private static MethodInfo updateThumbnailMethod = AccessTools.Method(theType, "UpdateThumbnailURL");
-		private static FieldInfo nameTextField = AccessTools.Field(theType, "_nameText");
-		private static FieldInfo detailTextField = AccessTools.Field(theType, "_detailText");
-		private static FieldInfo counterTextField = AccessTools.Field(theType, "_counterText");
-		private static FieldInfo deferredThumbnailField = AccessTools.Field(theType, "_deferredThumbnailUrl");
-		//private static FieldInfo thumbnailTextureField = AccessTools.Field(theType, "_thumbnailTexture");
+		private static MethodInfo updateThumbnailMethod = AccessTools.Method(worldThumbnailItemType, "UpdateThumbnailURL");
+		private static FieldInfo nameTextField = AccessTools.Field(worldThumbnailItemType, "_nameText");
+		private static FieldInfo detailTextField = AccessTools.Field(worldThumbnailItemType, "_detailText");
+		private static FieldInfo counterTextField = AccessTools.Field(worldThumbnailItemType, "_counterText");
+		private static FieldInfo deferredThumbnailField = AccessTools.Field(worldThumbnailItemType, "_deferredThumbnailUrl");
+		//private static FieldInfo thumbnailTextureField = AccessTools.Field(worldThumbnailItemType, "_thumbnailTexture");
 
-		// Userspace.UserspaceWorld.LocalUser?
-		// although maybe WorldThumbnailItem can exist in non-userspace worlds?
-		private static FrooxEngine.User localUser = null;
+		private static FrooxEngine.User LocalUser => Engine.Current.WorldManager.FocusedWorld.LocalUser ?? Userspace.UserspaceWorld.LocalUser;
 
 		private static List<SessionInfo> sessionsForWorldId = new List<SessionInfo>();
 
@@ -144,6 +174,34 @@ namespace WorldListSessionHider
 		//	}
 		//}
 
+		private static bool WorldIdFilter(SessionInfo sessionInfo)
+		{
+			if (string.IsNullOrWhiteSpace(sessionInfo.CorrespondingWorldId?.ToString())) return false;
+
+			string worldIds = Config.GetValue(WORLD_IDS);
+			if (string.IsNullOrWhiteSpace(worldIds)) return false;
+
+			foreach (var str in worldIds.Split(','))
+			{
+				string trimmed = str.Trim();
+				if (string.IsNullOrWhiteSpace(trimmed)) continue;
+
+				if (trimmed == sessionInfo.CorrespondingWorldId.ToString()) return true;
+
+				// try to turn it into a valid composite record id
+				string newstr = trimmed;
+				string recordSchemeStr = PlatformProfile.RESONITE.RecordScheme + ":///";
+				if (newstr.StartsWith(recordSchemeStr))
+				{
+					newstr = newstr.Remove(0, recordSchemeStr.Length);
+				}
+				newstr = newstr.Replace('/', ':');
+				if (newstr == sessionInfo.CorrespondingWorldId.ToString()) return true;
+			}
+
+			return false;
+		}
+
 		private static bool ShouldHideSession(SessionInfo sessionInfo, out HideReason? reason)
 		{
 			string sessionIds = Config.GetValue(SESSION_IDS);
@@ -158,18 +216,19 @@ namespace WorldListSessionHider
 			// If the session is not an opened world, but the local user is supposedly in the session then it's a stuck session
 			// or if the session has present users but hasn't updated for 30 minutes or more
 			if (Config.GetValue(HIDE_DEAD_SESSIONS) &&
-				(sessionInfo.SessionUsers.Any((SessionUser user) => (user.UserID ?? user.Username) == (localUser.UserID ?? localUser.UserName)) ||
+				(sessionInfo.SessionUsers.Any((SessionUser user) => (user.UserID ?? user.Username) == (LocalUser.UserID ?? LocalUser.UserName)) ||
 				(sessionInfo.SessionUsers.Any((SessionUser user) => user.IsPresent) && DateTime.UtcNow.Subtract(sessionInfo.LastUpdate).TotalMinutes >= 30)))
 			{
 				reason = HideReason.Stuck;
 				return true;
 			}
 			// Check the config values to see if this session should be filtered
-			else if (Config.GetValue(HIDE_FILTERED_SESSIONS) &&
-				((!string.IsNullOrWhiteSpace(sessionIds) && sessionIds.Split(',').Contains(sessionInfo.SessionId)) ||
-				((!string.IsNullOrWhiteSpace(hostUserIds) && hostUserIds.Split(',').Contains(sessionInfo.HostUserId)) ||
-				((!string.IsNullOrWhiteSpace(hostUsernames) && hostUsernames.Split(',').Contains(sessionInfo.HostUsername))
-				))))
+			else if (Config.GetValue(USE_FILTERS) &&
+				((!string.IsNullOrWhiteSpace(sessionIds) && sessionIds.Split(',').Select(str => str.Trim()).Contains(sessionInfo.SessionId)) ||
+				(!string.IsNullOrWhiteSpace(hostUserIds) && !string.IsNullOrWhiteSpace(sessionInfo.HostUserId) && hostUserIds.Split(',').Select(str => str.Trim()).Contains(sessionInfo.HostUserId)) ||
+				(!string.IsNullOrWhiteSpace(hostUsernames) && hostUsernames.Split(',').Select(str => str.Trim()).Contains(sessionInfo.HostUsername)) ||
+				WorldIdFilter(sessionInfo))
+				)
 			{
 				reason = HideReason.Filtered;
 				return true;
@@ -184,14 +243,8 @@ namespace WorldListSessionHider
 			return false;
 		}
 
-		private static void ProcessWorldThumbnailItem(WorldThumbnailItem worldThumbnailItem, string debugString = "")
+		private static void ProcessWorldThumbnailItem(LegacyWorldThumbnailItem worldThumbnailItem, string debugString = "")
 		{
-			// Ignore these comments
-			// If the number of sessions for a worldThumbnailItem keeps changing rapidly, use the first non-stuck and non-ended session as the primary one
-			//bool hasMultipleSessions = false;
-
-			localUser = worldThumbnailItem.LocalUser;
-
 			SessionInfo sessionInfo = Engine.Current.Cloud.Sessions.TryGetInfo(worldThumbnailItem.WorldOrSessionId);
 
 			if (sessionInfo == null)
@@ -201,14 +254,13 @@ namespace WorldListSessionHider
 				Engine.Current.Cloud.Sessions.GetSessionsForWorldId(RecordId.TryParse(worldThumbnailItem.WorldOrSessionId), sessionsForWorldId);
 				sessionInfo = (SessionInfo)getBestSessionMethod.Invoke(worldThumbnailItem, new object[] { sessionsForWorldId });
 				if (sessionInfo == null) return;
-				//hasMultipleSessions = true;
 			}
 
 			if (Config.GetValue(EXTRA_LOGGING))
 			{
 				Debug(new string('=', 30));
 				Debug("debugString: " + debugString);
-				Debug($"Host UserID: \"{sessionInfo.HostUserId}\" Host Username: \"{sessionInfo.HostUsername}\" SessionID: \"{sessionInfo.SessionId}\"");
+				Debug($"Host UserID: \"{sessionInfo.HostUserId}\" Host Username: \"{sessionInfo.HostUsername}\" SessionID: \"{sessionInfo.SessionId}\" WorldID: \"{sessionInfo.CorrespondingWorldId?.ToString() ?? "NULL"}\"");
 				foreach (string url in sessionInfo.SessionURLs)
 				{
 					Debug($"URL: {url}");
@@ -247,23 +299,11 @@ namespace WorldListSessionHider
 						break;
 				}
 				Debug(logString);
-				//if (hasMultipleSessions)
-				//{
-				//	HideReason? reason2;
-				//	if (sessions.All((SessionInfo sessionInfo2) => ShouldHideSession(sessionInfo, out reason2)))
-				//	{
-				//		flag = true;
-				//	}
-				//	else
-				//	{
-				//		flag = false;
-				//	}
-				//}
 				Hide(worldThumbnailItem, nameText, configValue && flag);
 			}
 		}
 
-		private static void Hide(WorldThumbnailItem worldThumbnailItem, string nameTextValue = "<i>[HIDDEN]</i>", bool hideCompletely = false)
+		private static void Hide(LegacyWorldThumbnailItem worldThumbnailItem, string nameTextValue = "<i>[HIDDEN]</i>", bool hideCompletely = false)
 		{
 			updateThumbnailMethod.Invoke(worldThumbnailItem, new object[] { OfficialAssets.Skyboxes.Thumbnails.NoThumbnail });
 			var nameText = (SyncRef<Text>)nameTextField.GetValue(worldThumbnailItem);
@@ -280,10 +320,10 @@ namespace WorldListSessionHider
 			}
 		}
 
-		[HarmonyPatch(typeof(WorldThumbnailItem), "UpdateInfo")]
+		[HarmonyPatch(typeof(LegacyWorldThumbnailItem), "UpdateInfo")]
 		class WorldListSessionHiderPatch
 		{
-			public static void Postfix(WorldThumbnailItem __instance, FrooxEngine.Store.Record record, IReadOnlyList<SessionInfo> sessions, IReadOnlyList<World> openedWorlds)
+			public static void Postfix(LegacyWorldThumbnailItem __instance, FrooxEngine.Store.Record record, IReadOnlyList<SessionInfo> sessions, IReadOnlyList<World> openedWorlds)
 			{
 				if (!Config.GetValue(MOD_ENABLED)) return;
 
@@ -291,10 +331,10 @@ namespace WorldListSessionHider
 			}
 		}
 
-		[HarmonyPatch(typeof(WorldThumbnailItem), "OnActivated")]
+		[HarmonyPatch(typeof(LegacyWorldThumbnailItem), "OnActivated")]
 		class WorldListSessionHiderPatch2
 		{
-			public static void Postfix(WorldThumbnailItem __instance)
+			public static void Postfix(LegacyWorldThumbnailItem __instance)
 			{
 				if (!Config.GetValue(MOD_ENABLED)) return;
 
